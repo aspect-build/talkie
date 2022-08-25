fab698dace0376ba22f2631015213038df543372
Dylan <dylan@aspect.dev>
2022-08-25 14:26:09 -0700

    [talkie] feat: add script to update go deps and targets (#67)
    * [talkie] feat: add script to update go deps and targets
    make sure that the bazel provided version of go is always used and that the commands are run in the correct order
    also make it consistent to run for devs and ci as well
    
    * [talkie] docs: describe what the talkie macro creates
    add examples of how to query and run the targets created by the talkie macro
    
    * fix: wrap tidy commands as a bazel command
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
    
    * [talkie] fix: clean up readme and wc command on mac
    
    * [talkie] fix: grammar in talkie readme
    
    * [talkie] fix: change wc and tidy to work on mac and linux
    
    * [talkie] fix: tidy script
    
    * Apply suggestions from code review
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
    Co-authored-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- README.md
+++ README.md
@@ -15,3 +15,31 @@ system instead of dealing with toil work.
 
 See the [examples](./examples) directory for a glimpse of how to use this
 framework.
+
+
+To update go targets and dependencies, make the required changes to the go files (import and use), then run:
+```
+bazel run tidy
+```
+
+To find all targets available from a talkie macro, run:
+```
+bazel query //{talkie macro folder name}:all 
+```
+ex: 
+```
+bazel query //examples/helloworld_http:all
+```
+
+should see response similar to: 
+```
+//examples/helloworld_http:helloworld_http_client
+//examples/helloworld_http:helloworld_http_entrypoints
+//examples/helloworld_http:helloworld_http_server
+```
+
+then, you can run the server to start the service:
+```
+bazel run //examples/helloworld_http:helloworld_http_server
+```
+This will start the service and expose all the endpoints as you created them.
---
+++ tidy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Aspect Build Systems Inc.
+# Original authors: Dylan Martin (dylan@aspect.dev)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit -o nounset -o pipefail
+
+# To add a new go dependency, make the required changes to the go files (import and use) and then
+# run this file.
+
+cd "${BUILD_WORKSPACE_DIRECTORY}"
+
+bazel run @go_sdk//:bin/go -- mod tidy
+bazel run //:gazelle_update_repos
+bazel run //:gazelle
+
+if [ $(git status --porcelain | wc -l) -gt 0 ]; then
+    >&2 echo "ERROR: files changed, commit them"
+    >&2 git diff
+    exit 1
+fi
