d8f7cd354d9d2d73a2177772d9d050847a7c98cd
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-09-15 17:24:35 -0700

    fix: use 2 spaces instead of 4 for yaml and json files (#267)
    * fix: use 2 spaces instead of 4 for yaml files
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
    
    * fix: use 2 spaces instead of 4 for json files
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- examples/workspace_status.sh
+++ examples/workspace_status.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset -o pipefail
 git_commit=$(git rev-parse HEAD)
 readonly git_commit
 
-cat <<EOF
+cat << EOF
 STABLE_GIT_COMMIT ${git_commit}
 STABLE_GIT_SHORT_COMMIT ${git_commit:0:8}
 STABLE_TALKIE_SERVICE_TAG ${git_commit}
