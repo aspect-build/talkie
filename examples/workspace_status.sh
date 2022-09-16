36e0281e6669d05a8d943eb91d01913b03606d2d
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-09-16 14:48:17 -0700

    fix: use dashes instead of pluses
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- examples/workspace_status.sh
+++ examples/workspace_status.sh
@@ -4,9 +4,14 @@ set -o errexit -o nounset -o pipefail
 
 git_commit=$(git rev-parse HEAD)
 readonly git_commit
+version=$(
+    git describe --tags --long --match="[0-9][0-9][0-9][0-9].[0-9][0-9]" \
+        | sed -e 's/-/./;s/-g/-/'
+)
+readonly version
 
 cat << EOF
 STABLE_GIT_COMMIT ${git_commit}
 STABLE_GIT_SHORT_COMMIT ${git_commit:0:8}
-STABLE_TALKIE_RELEASE_VERSION 0.0.0-${git_commit}
+STABLE_TALKIE_RELEASE_VERSION ${version}
 EOF
