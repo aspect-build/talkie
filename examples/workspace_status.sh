b7c11cb0e2e9eb6b8eb13c8601763fb76003df2b
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2023-08-11 04:31:16 -0700

    fix: missing script
    This is due to git subtree split work.
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- examples/workspace_status.sh
+++ examples/workspace_status.sh
@@ -5,12 +5,8 @@ set -o errexit -o nounset -o pipefail
 git_commit=$(git rev-parse HEAD)
 readonly git_commit
 
-# Semver compatible git describe: https://github.com/choffmeister/git-describe-semver
-version=$(cd ../.. && ./bazel/versioning/git-describe-semver --fallback v0.0.0)
-readonly version
-
 cat << EOF
 STABLE_GIT_COMMIT ${git_commit}
 STABLE_GIT_SHORT_COMMIT ${git_commit:0:8}
-STABLE_TALKIE_RELEASE_VERSION ${version}
+STABLE_TALKIE_RELEASE_VERSION 0.0.0
 EOF
